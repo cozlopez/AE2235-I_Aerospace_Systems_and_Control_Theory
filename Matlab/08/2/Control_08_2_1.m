f6794bc4e1ad9b5f8f732ce63eba5e2b8e9304e8
“Carlos <cozlopez@protonmail.com>
2024-07-11 14:41:13 +0200

    Finished 08_2 and added files for 08_3

--- Matlab/08/2/Control_08_2_1.m
+++ Matlab/08/2/Control_08_2_1.m
@@ -1,6 +1,10 @@
+clc
+clear
+clf
 %System parameters
-Kd = -0.6;
-
+Kr = -0.7;
+dt = 0.1;
+t = 0.1:dt:20;
 %System characteristics
 A = [-0.2, 0.06, 0, -1;...
       0,   0,    1,  0;...
@@ -13,24 +17,46 @@ B = [-0.01, 0.06;...
       -32,    5.4;...
       2.6,    -7];
 
-C = eye(4);
+C = [0  0   0   1];
 
-D = 0;
+D =[0   0];
 
 %Controller input 
 M =  [ 0 0 0 0;...
-     0 0 0 Kd];
+     0 0 0 Kr];
 
 
-%System
+%1st System
 sys = ss(A, B, C, D);
 
-closedSys = feedback(sys, M);
 
 
 
+%2nd system damping
+Hr1 = [0;Kr];
+sys1 = feedback(sys,Hr1);
 
-mat2str(closedSys.A)
-mat2str(closedSys.B)
-mat2str(closedSys.C)
-mat2str(closedSys.D)
+
+
+%3rd system washout+damping
+Hr2 = [0; Kr * ss(-0.5, 0.5, -1, 1)];
+sys2 = feedback(sys,Hr2);
+
+damp(sys)
+damp(sys1)
+damp(sys2)
+
+
+
+u = [ones(1, 10), zeros(1, length(t)-10);zeros(1,length(t))]';
+y0 = lsim(sys, u, t);
+y1 = lsim(sys1, u, t);
+y2 = lsim(sys2, u, t);
+% plotting, all three signals in different colors
+plot(t, [y0, y1, y2])
+% last (20s) value
+y0(end)
+y1(end)
+y2(end)
+legend("y0","y1","y2")
+
--- Matlab/08/2/Test.m
+++ Matlab/08/2/Test.m
@@ -1,64 +1,43 @@
-% Define the state-space matrices
-A = [-0.2 0.06 0 -1;
-     0 0 1 0;
-    -17 0 -3.8 1;
-     9.4 0 -0.4 -0.6];
-B = [-0.01 0.06;
-      0 0;
-    -32 5.4;
-      2.6 -7];
+%System parameters
+Kd = -0.6;
 
-% Output matrix to get all states
-C = eye(4);  % Identity matrix for all states as outputs
-D = zeros(4, 2);  % Corresponding to 2 inputs (elevator and rudder)
+%System characteristics
+A = [-0.2, 0.06, 0, -1;...
+      0,   0,    1,  0;...
+     -17,  0, -3.8,  1;...
+     9.4,  0, -0.4,-0.6];
 
-% Define the state-space system
-sys0 = ss(A, B, C, D);
 
-% Define the feedback matrix for yaw rate (r) to rudder input
-Kr = -0.7;
-K = [0 0 0 0;
-     0 0 0 Kr];  % Feedback matrix
+B = [-0.01, 0.06;...
+         0,    0;...
+      -32,    5.4;...
+      2.6,    -7];
 
-% Apply the feedback to create the closed-loop system
-sysc = feedback(sys0, K);  % Feedback from output to input
+C = [ 0   0   0   1]
+D = [ 0   0]
 
-% Calculate a step response for the closed-loop system
-[y_cl, t_cl] = step(sysc, 20);
+%Controllers input 
+M =  [0; Kd];
+     
+K = [0; Kd * ss(-0.5, 0.5, -1, 1)];
 
-% Plot the closed-loop response
-figure;
-subplot(4,1,1);
-plot(t_cl, y_cl(:,1));
-title('Closed-Loop Step Response');
-xlabel('Time (s)');
-ylabel('beta');
 
-subplot(4,1,2);
-plot(t_cl, y_cl(:,2));
-xlabel('Time (s)');
-ylabel('phi');
+%System
+sys = ss(A, B, C, D);
+closedSys1 = feedback(sys, M);
 
-subplot(4,1,3);
-plot(t_cl, y_cl(:,3));
-xlabel('Time (s)');
-ylabel('p');
+damp(sys)
+damp(closedSys1)
 
-subplot(4,1,4);
-plot(t_cl, y_cl(:,4));
-xlabel('Time (s)');
-ylabel('r');
+closedSys2 = feedback(sys, K);
 
-% Calculate a step response for the initial system (for comparison)
-[y, t] = step(sys0, 20);
+%Simulation
+t = 0.1:0.1:20;
+u = [ones(1, 10), zeros(1, length(t)-10);zeros(1,length(t))]';
 
-% Plot the response of the initial system
-figure;
-plot(t, y);
-title('Step Response for Initial System');
-xlabel('Time (s)');
-ylabel('Yaw Rate r');
-
-% Find the minimum value in the first second of the initial system
-min_val = min(y(t < 1));
-disp(['Minimum value in the first second: ', num2str(min_val)]);
+y = lsim(sys, u, t);
+y(end)
+y = lsim(closedSys1, u, t);
+y(end)
+y = lsim(closedSys2, u, t);
+y(end)
